389d1b3596eb0133034b8845f3e3abc860fc4e74
Vanilia <yuyan124@hotmail.com>
2024-04-19 13:36:13 +0800

    :new: feat: pwd_login

--- cardahoy-api/src/lib.rs
+++ cardahoy-api/src/lib.rs
@@ -3,17 +3,22 @@ use std::time::Duration;
 use anyhow::Result;
 use cardahoy_crypto as crypto;
 use cardahoy_utils as utils;
-use reqwest::{header, StatusCode};
+use reqwest::{
+    header::{self},
+    StatusCode,
+};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::{json, Value};
 pub mod analyze;
 pub mod buy_nft_detail;
 pub mod category_list;
+pub mod filter;
 pub mod game_config;
 pub mod market_home;
 pub mod market_secondary;
 pub mod nft;
 pub mod payment;
+pub mod sell;
 pub mod user_balance;
 
 #[derive(Deserialize, Debug)]
@@ -26,126 +31,6 @@ pub struct ResponseWrapper<T> {
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Continuity {
-    pub filter_name: String,
-    pub filter_id: u32,
-    pub start: u32,
-    pub step_size: u32,
-    pub end: u32,
-    pub max: u32,
-    pub min: u32,
-}
-
-impl Continuity {
-    pub fn level() -> Continuity {
-        Continuity {
-            filter_name: "Level".into(),
-            filter_id: 1,
-            start: 1,
-            step_size: 1,
-            end: 10,
-            max: 100,
-            min: 1,
-        }
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Discrete {
-    pub filter_name: String,
-    pub filter_value_list: Vec<DiscreteFilterValue>,
-    pub value_id_list: Vec<String>,
-    pub filter_id_list: Vec<String>,
-}
-
-impl Discrete {
-    pub fn filter_type(value_id_list: Vec<String>, filter_id_list: Vec<String>) -> Discrete {
-        Discrete {
-            filter_name: "Type".into(),
-            filter_value_list: vec![
-                DiscreteFilterValue::new("Leaders", "Leaders"),
-                DiscreteFilterValue::new("Members", "Members"),
-            ],
-            value_id_list,
-            filter_id_list,
-        }
-    }
-    pub fn faction(value_id_list: Vec<String>, filter_id_list: Vec<String>) -> Discrete {
-        Discrete {
-            filter_name: "Faction".into(),
-            filter_value_list: vec![
-                DiscreteFilterValue::new("Neutral", "Neutral"),
-                DiscreteFilterValue::new("Animal", "Animal"),
-                DiscreteFilterValue::new("Plant", "Plant"),
-                DiscreteFilterValue::new("Zombie", "Zombie"),
-                DiscreteFilterValue::new("Mech", "Mech"),
-                DiscreteFilterValue::new("Dragon", "Dragon"),
-            ],
-            value_id_list,
-            filter_id_list,
-        }
-    }
-
-    pub fn rarity(value_id_list: Vec<String>, filter_id_list: Vec<String>) -> Discrete {
-        Discrete {
-            filter_name: "Rarity".into(),
-            filter_value_list: vec![
-                DiscreteFilterValue::new("Common", "Common"),
-                DiscreteFilterValue::new("Rare", "Rare"),
-                DiscreteFilterValue::new("Epic", "Epic"),
-                DiscreteFilterValue::new("Legendary", "Legendary"),
-            ],
-            value_id_list,
-            filter_id_list,
-        }
-    }
-    pub fn foil(value_id_list: Vec<String>, filter_id_list: Vec<String>) -> Discrete {
-        Discrete {
-            filter_name: "Foil".into(),
-            filter_value_list: vec![
-                DiscreteFilterValue::new("Regular", "Regular"),
-                DiscreteFilterValue::new("Gold", "Gold"),
-            ],
-            value_id_list,
-            filter_id_list,
-        }
-    }
-
-    pub fn source(value_id_list: Vec<String>, filter_id_list: Vec<String>) -> Discrete {
-        Discrete {
-            filter_name: "Source".into(),
-            filter_value_list: vec![
-                DiscreteFilterValue::new("All", "All"),
-                DiscreteFilterValue::new("Ahoy Box", "Ahoy Box"),
-                DiscreteFilterValue::new("Ladder Chest", "Ladder Chest"),
-                DiscreteFilterValue::new("Alchemy", "Alchemy"),
-                DiscreteFilterValue::new("Reward", "Reward"),
-                DiscreteFilterValue::new("Season Box", "Season Box"),
-            ],
-            value_id_list,
-            filter_id_list,
-        }
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct DiscreteFilterValue {
-    pub value_name: String,
-    pub value_id: String,
-}
-impl DiscreteFilterValue {
-    pub fn new(value_name: &str, value_id: &str) -> DiscreteFilterValue {
-        DiscreteFilterValue {
-            value_name: value_name.into(),
-            value_id: value_id.into(),
-        }
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
 pub struct BuyNftParams {
     pub nonce: String,
     pub amount: u32,
@@ -153,6 +38,13 @@ pub struct BuyNftParams {
     pub payment_type: String,
     pub req_timestamp: i64,
     pub sale_aggregator_number: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PwdLoginParams {
+    pub password: String,
+    pub user_number: String,
 }
 
 const POST_MAX_RETRY: u32 = 4;
@@ -167,7 +59,7 @@ impl CardsAhoyApi {
         .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0")
         .default_headers({
             let mut headers = header::HeaderMap::new();
-            headers.insert("Authorization-Token", header::HeaderValue::from_str(&config.authorization)?);
+            headers.insert("Authorization-Token",header::HeaderValue::from_str(&config.authorization)?,);
             headers.insert("Client-App-Id", header::HeaderValue::from_str(&config.client_app_id)?);
             headers.insert("Cookie", header::HeaderValue::from_str(&config.cookies)?);
             headers.insert("Content-Type", header::HeaderValue::from_static("application/json"));
@@ -255,9 +147,17 @@ impl CardsAhoyApi {
 
             // 000000为成功， 不成功，返回错误信息。这里一般为 Not Logged，需要重新登录。
             if result.code != "000000" {
+                println!("network error");
                 println!("{:#?}", &result);
                 return Err(anyhow::anyhow!(result.msg));
             }
+            // if result.code == "100000" {
+            //     let token = self.pwd_login().await?;
+            //     return Err(anyhow::anyhow!(result.msg));
+            // } else if result.code != "000000" {
+            //     println!("{:#?}", &result);
+            //     return Err(anyhow::anyhow!(result.msg));
+            // }
 
             match result.data {
                 Some(data) => return Ok(data),
@@ -265,6 +165,10 @@ impl CardsAhoyApi {
             }
         }
         Err(anyhow::anyhow!("Max retries attempts reached"))
+    }
+
+    pub fn update_header(&mut self, key: &'static str, value: &str) -> Result<()> {
+        unimplemented!("update_header")
     }
 
     pub async fn query_secondary_filter_list(&self) -> Result<()> {
@@ -302,17 +206,9 @@ impl CardsAhoyApi {
     /// #     }
     /// # }
     /// ```
-    pub async fn buy_ntf_asset(&self, sale_aggregator_number: &str) -> Result<String> {
-        let params = BuyNftParams {
-            nonce: uuid::Uuid::new_v4().to_string(),
-            amount: 1,
-            password: "".into(),
-            payment_type: "Wallet".into(),
-            req_timestamp: utils::timestamp(),
-            sale_aggregator_number: sale_aggregator_number.into(),
-        };
-
-        let data = serde_json::to_string(&params)?;
+    ///
+    fn encrypt<T: Serialize>(&self, data: T) -> Result<(String, String)> {
+        let data = serde_json::to_string(&data)?;
         let key = crypto::random_key(16);
         let pem = std::fs::read_to_string("key.pem")?;
 
@@ -324,6 +220,34 @@ impl CardsAhoyApi {
         let encrypted_data = crypto::aes_ecb_encrypt(&data, &key)?;
         let enc_content = crypto::base64_encode(&encrypted_data);
         tracing::debug!("[AES] enc_content: {:?} ", enc_content);
+
+        Ok((enc_key, enc_content))
+    }
+
+    pub async fn buy_ntf_asset(&self, sale_aggregator_number: &str) -> Result<String> {
+        let params = BuyNftParams {
+            nonce: uuid::Uuid::new_v4().to_string(),
+            amount: 1,
+            password: "".into(),
+            payment_type: "Wallet".into(),
+            req_timestamp: utils::timestamp(),
+            sale_aggregator_number: sale_aggregator_number.into(),
+        };
+
+        // let data = serde_json::to_string(&params)?;
+        // let key = crypto::random_key(16);
+        // let pem = std::fs::read_to_string("key.pem")?;
+
+        // let encrypted = crypto::rsa_pkcs1_encrypt(&key, &pem)?;
+        // // let encrypted = crypto::rsa_encrypt(&key, pem)?;
+        // let enc_key = crypto::base64_encode(&encrypted);
+        // tracing::debug!("[RSA] enc_key: {:?} ", enc_key);
+
+        // let encrypted_data = crypto::aes_ecb_encrypt(&data, &key)?;
+        // let enc_content = crypto::base64_encode(&encrypted_data);
+        // tracing::debug!("[AES] enc_content: {:?} ", enc_content);
+
+        let (enc_key, enc_content) = self.encrypt::<BuyNftParams>(params)?;
 
         let payload = json!({
             "encContent": enc_content,
@@ -341,7 +265,42 @@ impl CardsAhoyApi {
         Ok(result)
     }
 
-    pub fn pwd_login(&self) -> Result<String> {
-        unimplemented!("pwd_login")
+    pub async fn pwd_login(&self) -> Result<String> {
+        // 返回的是  Authorization-Token
+        let password = "";
+        let params = PwdLoginParams {
+            password: password.into(),
+            user_number: "".into(),
+        };
+
+        // let data = serde_json::to_string(&params)?;
+        // let key = crypto::random_key(16);
+        // let pem = std::fs::read_to_string("key.pem")?;
+
+        // let encrypted = crypto::rsa_pkcs1_encrypt(&key, &pem)?;
+        // // let encrypted = crypto::rsa_encrypt(&key, pem)?;
+        // let enc_key = crypto::base64_encode(&encrypted);
+        // tracing::debug!("[RSA] enc_key: {:?} ", enc_key);
+
+        // let encrypted_data = crypto::aes_ecb_encrypt(&data, &key)?;
+        // let enc_content = crypto::base64_encode(&encrypted_data);
+        // tracing::debug!("[AES] enc_content: {:?} ", enc_content);
+
+        let (enc_key, enc_content) = self.encrypt::<PwdLoginParams>(params)?;
+
+        let payload = json!({
+            "encContent": enc_content,
+            "encKey": enc_key
+        });
+        tracing::debug!("[API] payload: {:?} ", payload);
+
+        let result = self
+            .post::<String>(
+                "https://game.metalist.io/api/marketOperate/buyNFTAsset",
+                payload,
+            )
+            .await?;
+
+        Ok(result)
     }
 }
